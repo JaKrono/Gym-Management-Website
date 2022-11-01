583822018b5f1a11c21e873244f9a5f8c4ba6228
Opal <mh.hashemi79@gmail.com>
2022-11-01 10:28:33 +0330

    add quasar

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './assets/main.css'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
 
+import 'quasar/src/css/index.sass'
+import './assets/main.css'
+import faIr from 'quasar/lang/fa-IR'
 const app = createApp(App)
 
 app.use(router)
 app.use(store as any)
+app.use(Quasar,{lang:faIr,plugins:{}})
 
 app.mount('#app')
