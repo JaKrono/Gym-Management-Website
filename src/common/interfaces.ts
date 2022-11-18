9a2e29f9650fa3e74b2e6d5888c8d83cb97c1d9b
Hessam Hosseini <shh2079@outlook.com>
2022-11-18 21:43:43 +0330

    add coach models in interface.ts

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,16 +1,35 @@
-export interface LoginModel{
-    username:string,
-    password:string
+export interface LoginModel {
+    username: string,
+    password: string
 }
 
-export interface SignupModel{
-    username:string,
-    email:string,
-    password:string
+export interface SignupModel {
+    username: string,
+    email: string,
+    password: string
 }
 
-export interface Notification{
-    message:string,
-    type:string,
-    timout:number
+export interface Notification {
+    message: string,
+    type: string,
+    timout: number
+}
+
+export interface CoachProfileModel {
+    id: string,
+    fullName: string,
+    description: string,
+    picUrl: string,
+    details: CoachDetailModel[],
+    achievements: CoachAchievementModel[]
+}
+
+export interface CoachDetailModel {
+    title: string,
+    value: string
+}
+
+export interface CoachAchievementModel {
+    title: string,
+    dateRange: string
 }
