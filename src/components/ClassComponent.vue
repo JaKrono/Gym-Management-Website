dcd398df51b37494ec9605c8cfa5657bf5c9987d
Hessam Hosseini <shh2079@outlook.com>
2022-11-27 11:10:33 +0330

    impliment class component

--- src/components/ClassComponent.vue
+++ src/components/ClassComponent.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="class-box">
-        hell
+        <div class="class-header">
+            <img class="edit-button" src="../assets/images/edit-icon.png" alt="edit button icon">
+            <div class="class-name main-font">بدن سازی</div>
+        </div>
+        <div class="class-detail">
+            <div class="class-detail-item secondary-font">مربی: سیدحسام حسینی</div>
+            <div class="class-detail-item secondary-font">ساعت: 18 - 20</div>
+            <div class="class-detail-item secondary-font">اعضا: 20</div>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -26,14 +34,57 @@ export default defineComponent({
     align-content: flex-start;
 }
 
+.main-font {
+    font-size: 18px;
+    font-weight: 700;
+}
+
+.secondary-font {
+    font-size: 16px;
+    font-weight: 500;
+}
+
 .class-box {
-    @extend .centerlize-item;
+    display: flex;
     flex-flow: row wrap;
+    align-content: flex-start;
     min-width: 260px;
     max-width: 260px;
-    height: 320px;
+    height: 160px;
     border-radius: 8px;
-    background-color: $secondary;
-    color: white;
+    background-color: white;
+    padding: 20px 15px;
+    color: $secondary;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+.class-header {
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+
+    .edit-button {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+    }
+}
+
+.class-detail {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: flex-start;
+    align-content: flex-start;
+    gap: 5px;
+
+    .class-detail-item {
+        width: 100%;
+        text-align: left;
+    }
 }
 </style>
