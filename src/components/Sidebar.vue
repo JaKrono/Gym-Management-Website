09191c9631b200b0a93119d31831d696023dfdf7
Hessam Hosseini <shh2079@outlook.com>
2022-12-01 23:19:31 +0330

    add club classes item to sidebar component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,9 @@
 <template>
     <q-drawer v-model="sidebarIsVisible" showIfAbove="md" side="left" bordered>
         <q-list padding>
-            <SidebarItemVue v-for="item in items" @itemClicked="itemClicked" class="q-mb-lg" :title="item.title" :path="item.path" :icon="item.icon" :id="item.id" :isSelected="isItemSelected(item.id)"></SidebarItemVue>
+            <SidebarItemVue v-for="item in items" @itemClicked="itemClicked" class="q-mb-lg" :title="item.title"
+                :path="item.path" :icon="item.icon" :id="item.id" :isSelected="isItemSelected(item.id)">
+            </SidebarItemVue>
         </q-list>
     </q-drawer>
 </template>
@@ -10,53 +12,59 @@ import { roles } from '@/common/enums';
 import { defineComponent } from 'vue';
 import { mapActions, mapMutations, mapState } from 'vuex';
 import SidebarItemVue from './SidebarItem.vue';
-    export default defineComponent({
-        components:{SidebarItemVue},
-        data:()=>({
-            selectedId:0,
-            items:[] as any,
-            width: window.innerWidth
-        }),
-        computed:{
-            sidebarIsVisible:{
-                get():any{
-                    return this.$store.state.global.sidebarIsVisible as any
-                },
-                set(value:boolean){
-                    this.$store.commit('global/changeSidebarVisibility', value)
-                }
+export default defineComponent({
+    components: { SidebarItemVue },
+    data: () => ({
+        selectedId: 0,
+        items: [] as any,
+        width: window.innerWidth
+    }),
+    computed: {
+        sidebarIsVisible: {
+            get(): any {
+                return this.$store.state.global.sidebarIsVisible as any
             },
-            ...mapState({
-                role: state => state.user.role
-            }),
-
-        },
-        mounted(){
-            // if(this.role === roles.owner){
-                this.items = [{id:1, title:'پروفایل', icon:'dashboard', path:'/dashboard'},{id:2, title:'مربی ها', icon:'person', path:'/coaches'}]
-            // }
-        },
-        methods:{
-            ...mapMutations({
-                hideSidebar:'global/hideSidebar',
-                showSidebar:'global/showSidebar',
-            }),
-            itemClicked(id:number){
-                this.selectedId= id;
-            },
-            isItemSelected(id:number){
-                return this.selectedId === id;
+            set(value: boolean) {
+                this.$store.commit('global/changeSidebarVisibility', value)
             }
         },
-        watch:{
-            '$q.screen.width':function(newValue){
-                if(newValue> 1080)
-                    this.showSidebar();
-                else{
-                    this.hideSidebar();
-                }
+        ...mapState({
+            role: state => state.user.role
+        }),
+
+    },
+    mounted() {
+        // if(this.role === roles.owner){
+        this.items = [
+            { id: 1, title: 'پروفایل', icon: 'dashboard', path: '/dashboard' },
+            { id: 2, title: 'مربی ها', icon: 'person', path: '/coaches' },
+            { id: 3, title: 'کلاس ها', icon: 'person', path: '/club-classes' }
+        ]
+        // }
+    },
+    methods: {
+        ...mapMutations({
+            hideSidebar: 'global/hideSidebar',
+            showSidebar: 'global/showSidebar',
+        }),
+        itemClicked(id: number) {
+            this.selectedId = id;
+        },
+        isItemSelected(id: number) {
+            return this.selectedId === id;
+        }
+    },
+    watch: {
+        '$q.screen.width': function (newValue) {
+            if (newValue > 1080)
+                this.showSidebar();
+            else {
+                this.hideSidebar();
             }
         }
-    })
+    }
+})
 </script>
-<style></style>
+<style>
+
+</style>
