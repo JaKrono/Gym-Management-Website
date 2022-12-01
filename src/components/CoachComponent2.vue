608175eafd953664abeb5708ccae49672b702d43
Hessam Hosseini <shh2079@outlook.com>
2022-12-01 19:32:56 +0330

    add prop to CoachComponent2 & bind data

--- src/components/CoachComponent2.vue
+++ src/components/CoachComponent2.vue
@@ -6,12 +6,12 @@
                 <q-btn v-if="isInAddState" icon="add" flat color="white" rounded fab></q-btn>
             </div>
             <div class="image-cn">
-                <img src="player.jpg" class="user-image"/>
+                <img v-bind:src="(coachObject.picUrl) ? coachObject.picUrl : 'player.jpg'" class="user-image" />
             </div>
         </div>
         <div class="col-lg-7 q-pa-md second-section row column ">
-            
-            <div style="font-size:1.3em">بیژن مرتضوی</div>
+
+            <div style="font-size:1.3em">{{ coachObject.fullName }}</div>
             <div>مربی</div>
             <q-space></q-space>
             <q-btn @click="showCoachClicked" color="primary" outline>مشاهده پروفایل</q-btn>
@@ -21,43 +21,47 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-    export default defineComponent({
-        props:['isInAddState'],
-        mounted(){
-            console.log(this.isInAddState);
-            
-        },
-        methods: {
-            showCoachClicked() {
-                this.$router.push('/coach-profile')
-            }
-        }
-    })
-</script>
-<style scoped lang="scss">
-    .coach-card{
-        box-shadow: 0 0 4px 0.3px #000;
-        .first-section{
-            position: relative;
-            height: 150px;
-        }
-        .image-cn{
-            width: 100px;
-            height: 100px;
-            overflow: hidden;
-            border-radius: 50%;
-            position: absolute;
-            bottom: -30px;
-            right: 10px;
-            .user-image{
-                width:100px;
-                height:100px;
-                object-fit: cover;
-                bottom: 0;
-            }
-        }
-        .second-section{
-            height: 300px;
+export default defineComponent({
+    props: ['isInAddState', 'coachObject'],
+    mounted() {
+        console.log(this.isInAddState);
+
+    },
+    methods: {
+        showCoachClicked() {
+            this.$router.push({ path: '/coach-profile', query: { coachId: this.coachObject.id, isCoach: 'false' } })
         }
     }
+})
+</script>
+<style scoped lang="scss">
+.coach-card {
+    box-shadow: 0 0 4px 0.3px #000;
+
+    .first-section {
+        position: relative;
+        height: 150px;
+    }
+
+    .image-cn {
+        width: 100px;
+        height: 100px;
+        overflow: hidden;
+        border-radius: 50%;
+        position: absolute;
+        bottom: -30px;
+        right: 10px;
+
+        .user-image {
+            width: 100px;
+            height: 100px;
+            object-fit: cover;
+            bottom: 0;
+        }
+    }
+
+    .second-section {
+        height: 300px;
+    }
+}
 </style> 
