0b68e2cb4883886482ace4232fe1feb420f9b068
Hessam Hosseini <shh2079@outlook.com>
2022-12-02 17:06:26 +0330

    change authentication API urls

--- src/repositories/authentication.ts
+++ src/repositories/authentication.ts
@@ -1,16 +1,16 @@
 import type { LoginModel, SignupModel } from "@/common/interfaces";
 import client from "./client/axios";
-export default{
+export default {
 
-    login(loginModel:LoginModel){
-        const endpoint = '/auth/jwt/create/'
-        const response = client.post(endpoint,loginModel)
+    login(loginModel: LoginModel) {
+        const endpoint = '/api/auth/jwt/create/'
+        const response = client.post(endpoint, loginModel)
         return response;
     },
 
-    signup(signupModel:SignupModel){
-        const endpoint = '/user/signup/'
-        const response = client.post(endpoint,signupModel)
+    signup(signupModel: SignupModel) {
+        const endpoint = '/api/user/signup/'
+        const response = client.post(endpoint, signupModel)
         return response;
     }
 }
