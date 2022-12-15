21431774340ccfa3ee664fad87f5772ad4cc2a97
Jack Krono <mousavi20k@gmail.com>
2022-12-16 02:59:39 +0330

    fix: change GymCardComponent
     + imrove UI
     + add address and phone number:
          props:
             address: boolean
                show address in card
             phoneNumber: boolean
                show phone number in card
          usage:
             <GymCardComponent :model="GymModel" address phoneNumber></GymCardComponent>

--- src/components/Gym/GymCardComponent.vue
+++ src/components/Gym/GymCardComponent.vue
@@ -3,8 +3,8 @@
       <q-card bordered>
          <q-card-section class="q-pa-none">
 
-            <q-card-section class="q-pa-none">
-               <q-img :src="model.picUrl" alt="Gym Image" :ratio="$q.screen.lt.sm ? 3 / 1 : 16 / 9" fit="cover">
+            <q-card-section class="q-px-sm q-pt-sm q-pb-none">
+               <q-img class="rounded-borders" :src="model.picUrl" alt="Gym Image" :ratio="$q.screen.lt.sm ? 3 / 1 : 16 / 9" fit="cover">
                   <template v-slot:error>
                      <q-img class="q-pa-lg" alt="Dumbell Image" src="dumbell.svg" position="50% 103%" :ratio="16 / 9"
                         :img-style="{ filter: 'blur(4px)' }"></q-img>
@@ -13,9 +13,17 @@
                   </template>
                </q-img>
             </q-card-section>
-            <!-- HEIGHT -->
-            <q-card-section class="row q-pa-sm" style="height: 80px">
-               <p class="col-12 font-size-up-1 text-weight-bold ellipsis-2-lines">{{ model.name }}</p>
+            
+            <q-card-section class="row q-pa-sm">
+               <p class="col-12 font-size-up-1 text-weight-bold ellipsis-2-lines" style="height: 42px">{{ model.name }}</p>
+               <div class="flex no-wrap col-12 q-pt-sm" v-if="address">
+                  <q-icon name="place" class="font-size-up-4" style="margin: 0px -4px 0px 4px"></q-icon>
+                  <p class="ellipsis-2-lines" style="height: 42px">{{ model.address }}</p>
+               </div>
+               <div class="flex no-wrap col-12 q-pt-sm" v-if="phoneNumber">
+                  <q-icon name="call" class="font-size-up-3" style="margin: 0px -2px 0px 4px"></q-icon>
+                  <p class="ellipsis-1" style="height: 21px">{{ model.phone }}</p>
+               </div>
             </q-card-section>
 
             <q-card-actions class="flex" align="right">
@@ -30,10 +38,16 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+import type { GymModel } from '@/common/interfaces';
 export default defineComponent({
    data: () => ({
    }),
-   props: [ 'model' ],
+   props: {
+      model: Object as PropType<GymModel>,
+      address: Boolean,
+      phoneNumber: Boolean
+   },
    methods: {
       viewGymDetailClicked() {
          // this.$emit('viewGymProfile', )
