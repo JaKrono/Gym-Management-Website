afeaf1634b64949eef56c5f9b13bd223eba9a1ad
Opal <mh.hashemi79@gmail.com>
2022-12-19 20:10:04 +0330

    fix: coach profile

--- src/store/modules/coach.ts
+++ src/store/modules/coach.ts
@@ -54,6 +54,19 @@ export default {
                 return false
             }
         },
+
+        async getCoachDetail({ commit, dispatch }:any) {
+            const response = await Coach.getCoachDetail(store.state.user.user.role_id)
+            debugger;
+            if (response.status === 200) {
+               commit('setCoachDetail', response.data)
+               return true
+            } else {
+               dispatch('notification/showNotification', { message: response?.data.detail || "خطا در گرفتن اطلاعات مربی", type: 'negative', timeout: 2000 }, { root: true })
+               return false
+            }
+         },
+         
         async getGymCardInformation({ dispatch }: any, obj: { coachId: number, gymId: number }) {
             const response = await Coach.getGymCardInformation(obj.coachId, obj.gymId)
             if (response.status === 200) {
@@ -90,7 +103,15 @@ export default {
                 dispatch('notification/showNotification', { message: response?.data.detail || "خطا در برقراری ازتباط", type: 'negative', timeout: 2000 }, { root: true })
                 return false;
             }
-        }
+        },
+        async saveCoach({state, commit, dispatch}:any,coachModel){
+            const response = await Coach.saveCoach(store.state.user.user.role_id, coachModel)
+            dispatch("user/getUser",null,{root:true})
+            if(response.status === 200){
+               dispatch('notification/showNotification', { message: response?.data.detail || "اطلاعات با موفقیت ذخیره شد.", type: 'positive', timeout: 2000 }, { root: true })
+               return false
+            }
+         }
     },
 
 }
