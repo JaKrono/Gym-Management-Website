87f93a30c05e562994801d5fd502c14a753c4d85
Opal <mh.hashemi79@gmail.com>
2022-12-19 19:22:45 +0330

    feat: coach panel

--- src/store/modules/coach.ts
+++ src/store/modules/coach.ts
@@ -4,7 +4,20 @@ export default {
     namespaced: true,
     state: () => ({
         coachId: -1,
-        clubs: []
+        clubs: [],
+        coach:{
+            user_id: -1,
+            description: null,
+            detail_set: [],
+            achievement_set: [],
+            user: {
+              username: "",
+              gender: null,
+              picUrl: null,
+              first_name: null,
+              last_name: null
+        }
+    }
     }),
     mutations: {
         setCoachId(state: any, coachId: number) {
@@ -13,19 +26,25 @@ export default {
         },
         setClubs(state, clubs) {
             state.clubs = clubs
-        }
+        },
+        setCoachDetail(state:any, coachModel){
+            state.coach = coachModel
+      },
     },
-    actions: {
-        async getCoachId({ commit, dispatch }: any) {
+
+    actions:{
+
+         async getCoachId({ state, commit, dispatch }: any) {
             const response = await Coach.getCoachId(store.state.user.userId)
             if (response.status === 200) {
-                commit('setCoachId', response.data.role_id)
-                return true
-            } else {
-                dispatch('notification/showNotification', { message: response?.data.detail || "خطا در گرفتن اطلاعات مربی", type: 'negative', timeout: 2000 }, { root: true })
-                return false
-            }
-        },
+               commit('setCoachId', response.data.role_id)
+
+           } else {
+               dispatch('notification/showNotification', { message: response?.data.detail || "خطا در گرفتن اطلاعات مربی", type: 'negative', timeout: 2000 }, { root: true })
+           }
+            state.coachId = coachId
+            console.log(store.state.user.userId, state.coachId)
+         },
         async getAcceptedGyms({ commit, dispatch }: any, coachId: number) {
             const response = await Coach.getAcceptedGyms(coachId)
             if (response.status === 200) {
@@ -43,7 +62,27 @@ export default {
                 dispatch('notification/showNotification', { message: response?.data.detail || "خطا در برقراری ازتباط", type: 'negative', timeout: 2000 }, { root: true })
                 return false;
             }
-        }
+        },
+        async getCoachDetail({ commit, dispatch }:any) {
+         const response = await Coach.getCoachDetail(store.state.user.user.role_id)
+         debugger;
+         if (response.status === 200) {
+            commit('setCoachDetail', response.data)
+            return true
+         } else {
+            dispatch('notification/showNotification', { message: response?.data.detail || "خطا در گرفتن اطلاعات مربی", type: 'negative', timeout: 2000 }, { root: true })
+            return false
+         }
+      },
+
+      async saveCoach({state, commit, dispatch}:any,coachModel){
+         const response = await Coach.saveCoach(store.state.user.user.role_id, coachModel)
+         dispatch("user/getUser",null,{root:true})
+         if(response.status === 200){
+            dispatch('notification/showNotification', { message: response?.data.detail || "اطلاعات با موفقیت ذخیره شد.", type: 'positive', timeout: 2000 }, { root: true })
+            return false
+         }
+      }
     },
 
 }
