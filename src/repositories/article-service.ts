8a3a48255bc6345d250ac56f7d76be22c1928cfc
Hessam Hosseini <shh2079@outlook.com>
2023-01-22 23:07:11 +0330

    fix bug in core policy

--- src/repositories/article-service.ts
+++ src/repositories/article-service.ts
@@ -3,19 +3,19 @@ import client from "./client/axios";
 export default {
 
     getArticleList() {
-        const endpoint = `/article/article`;
+        const endpoint = `/article/article/`;
         const response = client.get(endpoint);
         return response;
     },
 
     editArticle(writerId: string, articleObject: ChangeArticleDetailModel) {
-        const endpoint = `/article/article/${writerId}`;
+        const endpoint = `/article/article/${writerId}/`;
         const response = client.put(endpoint, articleObject);
         return response;
     },
 
     addArticle(newArticle: ChangeArticleDetailModel) {
-        const endpoint = `/article/article`;
+        const endpoint = `/article/article/`;
         const response = client.post(endpoint, newArticle);
         return response;
     }
