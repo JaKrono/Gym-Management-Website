d7998e5b55e9bf9d42874ec5b5754ac645d60288
Jack Krono <mousavi20k@gmail.com>
2023-01-23 15:28:02 +0330

    wip: add CourseList

--- src/common/category-list.ts
+++ src/common/category-list.ts
@@ -8,3 +8,12 @@ export const CategoryList: CategoryModel[] = [
     { id: 5, title: 'شنا', color: 'green-10' },
     { id: 6, title: 'تجهیزات ورزشی', color: 'blue-5' }
 ]
+
+export const CourseList: {}[] = [
+    { id: 1, title: 'ژیمناستیک', color: 'indigo-10' },
+    { id: 2, title: 'بدنسازی', color: 'teal-10' },
+    { id: 3, title: 'TRX', color: 'brown-10' },
+    { id: 4, title: 'کشتی', color: 'brown-4' },
+    { id: 5, title: 'شنا', color: 'green-10' },
+    { id: 6, title: 'یوگا', color: 'blue-5' }
+]
